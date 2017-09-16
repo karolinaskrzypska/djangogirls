045f287d8247663529e61cfbd933d85f7c9bae44
Karolina <karolina.skrzypska@gmail.com>
2017-09-16 18:31:24 +0200

    Fix allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fecn2*r08(fo+)h&#(h$$xlnk9p=wak+%%-asykuuuz^3vn+)v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['karolinaskrzypska.pythonanywhere.com']
 
 
 # Application definition
